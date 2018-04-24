16a134693c9e4c6526ca7c062793488a091ae4af
poltorres0999 <eichgammer@gmail.com>
2018-04-25 01:15:51 +0200

    Problem5

--- src/Composite/Problem_5/ComplexTask.java
+++ src/Composite/Problem_5/ComplexTask.java
@@ -1,33 +1,4 @@
 package Composite.Problem_5;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class ComplexTask implements Task {
-
-    private Money cost;
-    private int duration;
-    private List<Task> taskList = new ArrayList<>();
-
-    @Override
-    public Money costInEuros() {
-
-        for (Task t: this.taskList) {
-            this.cost.increase(t.costInEuros().getCurrency());
-        }
-        return this.cost;
-    }
-
-    @Override
-    public int durationInDays() {
-        for (Task t: this.taskList) {
-            this.duration += t.durationInDays();
-        }
-
-        return this.duration;
-    }
-
-    public void addSubTask(Task task) {
-        this.taskList.add(task);
-    }
+public abstract class ComplexTask implements Task {
 }
