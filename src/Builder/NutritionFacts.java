2e7e9b477fe69c5a996a53f9ed6361b95efb230d
poltorres0999 <eichgammer@gmail.com>
2018-05-07 22:25:35 +0200

    builder

--- src/Builder/NutritionFacts.java
+++ src/Builder/NutritionFacts.java
@@ -1,3 +1,5 @@
+package Builder;
+
 public class NutritionFacts {
 	//Requiered parameters
 	private final int servingSize;
@@ -14,10 +16,10 @@ public class NutritionFacts {
 		this.calories = builder.calories;
 		this.fat = builder.fat;
 		this.sodium = builder.sodium;
-		this.carbohydrate;
+		this.carbohydrate = builder.carbohydrate;
 	}
 
-	//Other methods shuld be implemented,
+	//Other methods sould be implemented,
 	// but just for showing the pattern is not necessary
 
 	//We need a static class builder to create the instances of NutritonFacts
@@ -32,7 +34,7 @@ public class NutritionFacts {
 		private int sodium;
 		private int carbohydrate;
 
-		public NutritionFactsBuilder (int servingSize, int servings) {
+		public NutritionFactsBuilder(int servingSize, int servings)  {
 			this.servingSize = servingSize;
 			this.servings = servings;
 		}
@@ -41,12 +43,12 @@ public class NutritionFacts {
 			this.calories = calories;
 			return this;
 		}
-		
+
 		public NutritionFactsBuilder setFat(int fat) {
 			this.fat = fat;
 			return this;
 		}
-		
+
 		public NutritionFactsBuilder setSodium(int sodium) {
 			this.sodium = sodium;
 			return this;
@@ -57,8 +59,27 @@ public class NutritionFacts {
 			return this;
 		}
 
-		public NutritionFacts build () {
-			return new NutritionFacts(this)
+		public NutritionFacts build() {
+
+		    if (this.fat == 0) {
+		        throw new RuntimeException("Fat should be > 0");
+            }
+
+            if (this.sodium == 0) {
+		        throw new RuntimeException("sodium should be > 0");
+            }
+
+            if (this.calories == 0) {
+                throw new RuntimeException("calories should be > 0");
+            }
+
+            if (this.carbohydrate == 0) {
+                throw new RuntimeException("carbohydrate should be > 0");
+            }
+
+			return new NutritionFacts(this);
 		}
+	}
+}
 		
 		
